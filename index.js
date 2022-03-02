7c719ba238cec2d4e46f2663521fe7128eef79f5
fazikme <fazikme@gmail.com>
2022-03-02 16:02:52 +0300

    funksiya

---
+++ index.js
@@ -0,0 +1,17 @@
+'use strict'
+
+// const text = "Hello world";
+
+// const arr = [1, 2, 3, 4, 5]
+
+// console.log(arr.length);
+
+// console.log(text.indexOf("w"));
+
+// console.log(text.slice(5, 11));
+
+// console.log(text.substring(4));
+
+const number = "99.4445";
+
+console.log(Math.round(number));
